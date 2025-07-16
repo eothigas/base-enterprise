d823a7488295c5c44bb5a7a343c8619b27aa0e3b
Thiago <thi16thiago@gmail.com>
2025-07-15 22:48:52 -0300

    Ajustando e criando novas seções

--- index.php
+++ index.php
@@ -86,6 +86,154 @@
                 </div>
             </div>
         </section>
+        <section id="services">
+            <div class="container-fluid">
+                <img src="<?php echo $url;?>imagens/main/services/square-number.svg" alt="img fundo serviço" class="img-responsive bg-service">
+                <h2>Nossos Serviços</h2>
+                <div class="flex align-items-start justify-content-between gap-5">
+                    <div class="col-lg-4 col-md-4 col-sm-10">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum, id!</p>
+
+                        <div class="button-service">
+                            <div class="flex btns active-btn" >
+                                <div class="num">
+                                    <span class="">01</span>
+                                </div>
+                                <div class="text">
+                                    <h4>Serviço 1</h4>
+                                </div>
+                                <div class="icon">
+                                    <i class="bi bi-arrow-right"></i>
+                                </div>
+                            </div>
+                            <div class="flex btns">
+                                <div class="num">
+                                    <span class="active">02</span>
+                                </div>
+                                <div class="text">
+                                    <h4>Serviço 2</h4>
+                                </div>
+                                <div class="icon">
+                                    <i class="bi bi-arrow-right"></i>
+                                </div>
+                            </div>
+                            <div class="flex btns">
+                                <div class="num">
+                                    <span class="active">03</span>
+                                </div>
+                                <div class="text">
+                                    <h4>Serviço 3</h4>
+                                </div>
+                                <div class="icon">
+                                    <i class="bi bi-arrow-right"></i>
+                                </div>
+                            </div>
+                            <div class="flex btns">
+                                <div class="num">
+                                    <span class="active">04</span>
+                                </div>
+                                <div class="text">
+                                    <h4>Serviço 4</h4>
+                                </div>
+                                <div class="icon">
+                                    <i class="bi bi-arrow-right"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-4 col-sm-10">
+                        <img src="<?php echo $url;?>imagens/main/services/service1.webp" alt="Imagem de Serviços" class="img-responsive service-img">
+                    </div>
+                    <div class="col-lg-3 col-md-3 col-sm-10">
+                        <div class="flex align-items-center justify-content-center flex-column gap-3">
+                            <div class="col-lg-12 col-md-12 col-sm-12">
+                               <div class="card-service">
+                                    <h4>Quer saber como nosso serviço funciona?</h4>
+                                    <div class="flex link-card justify-content-start">
+                                        <a href="#" class="button">
+                                            Saiba Mais
+                                            <span class="button__icon-wrapper">
+                                                <svg
+                                                viewBox="0 0 14 15"
+                                                fill="none"
+                                                xmlns="http://www.w3.org/2000/svg"
+                                                class="button__icon-svg"
+                                                width="10"
+                                                >
+                                                <path
+                                                    d="M13.376 11.552l-.264-10.44-10.44-.24.024 2.28 6.96-.048L.2 12.56l1.488 1.488 9.432-9.432-.048 6.912 2.304.024z"
+                                                    fill="currentColor"
+                                                ></path>
+                                                </svg>
+
+                                                <svg
+                                                viewBox="0 0 14 15"
+                                                fill="none"
+                                                width="10"
+                                                xmlns="http://www.w3.org/2000/svg"
+                                                class="button__icon-svg button__icon-svg--copy"
+                                                >
+                                                <path
+                                                    d="M13.376 11.552l-.264-10.44-10.44-.24.024 2.28 6.96-.048L.2 12.56l1.488 1.488 9.432-9.432-.048 6.912 2.304.024z"
+                                                    fill="currentColor"
+                                                ></path>
+                                                </svg>
+                                            </span>
+                                            
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-12">
+                                <div class="card-service-alt">
+                                    <h4>Procurando serviço de qualidade?</h4>
+                                    <div class="flex link-card justify-content-start">
+                                        <a href="#" class="button">
+                                            Contate-nos
+                                            <span class="button__icon-wrapper">
+                                                <svg
+                                                viewBox="0 0 14 15"
+                                                fill="none"
+                                                xmlns="http://www.w3.org/2000/svg"
+                                                class="button__icon-svg"
+                                                width="10"
+                                                >
+                                                <path
+                                                    d="M13.376 11.552l-.264-10.44-10.44-.24.024 2.28 6.96-.048L.2 12.56l1.488 1.488 9.432-9.432-.048 6.912 2.304.024z"
+                                                    fill="currentColor"
+                                                ></path>
+                                                </svg>
+
+                                                <svg
+                                                viewBox="0 0 14 15"
+                                                fill="none"
+                                                width="10"
+                                                xmlns="http://www.w3.org/2000/svg"
+                                                class="button__icon-svg button__icon-svg--copy"
+                                                >
+                                                <path
+                                                    d="M13.376 11.552l-.264-10.44-10.44-.24.024 2.28 6.96-.048L.2 12.56l1.488 1.488 9.432-9.432-.048 6.912 2.304.024z"
+                                                    fill="currentColor"
+                                                ></path>
+                                                </svg>
+                                            </span>
+                                            
+                                        </a>
+                                    </div>
+                                </div>
+                                
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <img src="<?php echo $url;?>imagens/main/services/square-number.svg" alt="img fundo serviço" class="img-responsive bg-service-alt">
+            </div>
+        </section>
+        <section id="team">
+            <div class="container">
+                <h2>seção time</h2>
+            </div>
+        </section>
     </main>
 
     
